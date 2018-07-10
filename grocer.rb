0026945bb8f9d515d5026a2eda0651b0c6d13f7c
Jon Schulman <schulman99@gmail.com>
2018-07-10 16:36:58 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,29 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+new_cart = {}
+  cart.each do |in_cart|
+    in_cart.each do |food, info|
+      new_cart[food] ||= info
+      new_cart[food].merge!(:count => cart.count(in_cart))
+    end
+  end
+new_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+# cart_with_coupons = {}
+# cup_item = nil
+coupons.each do |has_coupon|
+  with_coupon = has_coupon[:item]
+  binding.pry
+  if cart[with_coupon] && cart[with_coupon][:count] >= has_coupon[:num]
+    if cart["#{with_coupon} W/ COUPON"]
+      if cart["#{with_coupon} W/ COUPON"]
+end
+
+
+# cart_with_coupons
 end
 
 def apply_clearance(cart)
