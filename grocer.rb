a55fc4f11bf730db205a3c73951431cf808b2f52
Jon Schulman <schulman99@gmail.com>
2018-07-10 17:14:09 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -17,21 +17,6 @@ def apply_coupons(cart, coupons)
 # binding.pry
 coupons.each do |has_coupon|
   with_coupon = has_coupon[:item]
-  if cart[with_coupon] && cart[with_coupon][:count] >= has_coupon[:num]
-    if cart["#{with_coupon} W/COUPON"]
-      cart["#{with_coupon} W/COUPON"][:count] += 1
-    else
-      cart["#{with_coupon} W/COUPON"] = {:count => 1, :price => has_coupon[:cost]}
-      cart["#{with_coupon} W/COUPON"][:clearance] = cart[with_coupon][:clearance]
-    end
-    cart[with_coupon][:count] -= has_coupon[:num]
-  end
-end
-cart
-end
-
-coupons.each do |has_coupon|
-  with_coupon = has_coupon[:item]
     if cart[with_coupon] && cart[with_coupon][:count] >= has_coupon[:num]
       if cart["#{with_coupon} W/COUPON"] #!= nil
         cart["#{with_coupon} W/ COUPON"][:count] += 1
