4b3b3b68788527446c374d7d84289d4e897f0e95
Jon Schulman <schulman99@gmail.com>
2018-07-10 18:05:57 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -40,5 +40,9 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  binding.pry
+  small_cart = consolidate_cart(cart)
+  coupons_applied = apply_coupons(small_cart, coupons)
+  checkout_cart = apply_coupons(coupons_applied)
+  total_cost = nil
+  pry
 end
